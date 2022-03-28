c948cc1a278c73a7df365579a25c319c30a5a9ab
Charles Graham <cpg55850@ucmo.edu>
2022-03-28 13:55:30 -0500

    User profile uses API

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,108 +1,126 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import Avatar from '../../components/Avatar'
 import ViewWuphfs from '../../components/ViewWuphfs'
 import Button from '../../components/Button'
 import tempPosts from '../../assets/tempPosts'
 import styled from 'styled-components'
+import axios from 'axios'
+import moment from 'moment'
 
 const tempUser = {
-  username: 'John Doe',
-  joinDate: '3 weeks ago',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere vitae enim risus consectetur sed at vitae lectus. Amet purus massa accumsan in. Facilisis nec aliquet ac nulla. Odio et eros, pretium lacus, nulla.',
+	username: 'John Doe',
+	joinDate: '3 weeks ago',
+	bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere vitae enim risus consectetur sed at vitae lectus. Amet purus massa accumsan in. Facilisis nec aliquet ac nulla. Odio et eros, pretium lacus, nulla.',
 }
 
 function profilePage(props) {
-  return (
-    <div>
-      {/* this is a temporary navbar, replace */}
-      <Nav as="nav">WUPHF</Nav>
+	const router = useRouter()
+	const { id } = router.query
+	const [user, setUser] = useState()
 
-      <TopContainer>
-        <Banner />
+	useEffect(() => {
+		const getUser = async () => {
+			const { data } = await axios.get('../api/user')
+			setUser(data)
+		}
 
-        <Header>
-          <Text>
-            <Username as="h1">{tempUser.username}</Username>
-            <Joined as="h3">Joined {tempUser.joinDate}</Joined>
-          </Text>
+		getUser()
+	}, [])
 
-          <AvatarWrapper>
-            <Avatar
-              username="John Doe"
-              profileImageUrl="sample.jpg"
-              size="large"
-            />
-          </AvatarWrapper>
+	console.log(id)
 
-          <Buttons>
-            <Button variant="primary">...</Button>
-            <Button variant="primary">...</Button>
-            <Button variant="primary">Follow</Button>
-          </Buttons>
-        </Header>
+	return (
+		<div>
+			{/* this is a temporary navbar, replace */}
+			<Nav as='nav'>WUPHF</Nav>
 
-        {/* //replace below with user input Biography in settings page */}
-        <Bio>{tempUser.bio}</Bio>
-      </TopContainer>
-      <ViewWuphfs posts={tempPosts} />
-    </div>
-  )
+			<TopContainer>
+				<Banner />
+
+				<Header>
+					<Text>
+						<Username as='h1'>{user?.userName}</Username>
+						<Joined as='h3'>Joined {moment(user?.createdAt).fromNow()}</Joined>
+					</Text>
+
+					<AvatarWrapper>
+						<Avatar
+							username='John Doe'
+							profileImageUrl='sample.jpg'
+							size='large'
+						/>
+					</AvatarWrapper>
+
+					<Buttons>
+						<Button variant='primary'>...</Button>
+						<Button variant='primary'>...</Button>
+						<Button variant='primary'>Follow</Button>
+					</Buttons>
+				</Header>
+
+				{/* //replace below with user input Biography in settings page */}
+				<Bio>{user?.bio}</Bio>
+			</TopContainer>
+			<ViewWuphfs posts={tempPosts} />
+		</div>
+	)
 }
 
 const Nav = styled.nav`
-  font-weight: bolder;
-  font-size: larger;
-  display: flex;
-  align-items: center;
-  margin-left: 10px;
-  padding-top: 15px;
-  padding-bottom: 15px;
+	font-weight: bolder;
+	font-size: larger;
+	display: flex;
+	align-items: center;
+	margin-left: 10px;
+	padding-top: 15px;
+	padding-bottom: 15px;
 `
 
 const TopContainer = styled.div`
-  position: relative;
+	position: relative;
 `
 
 const Banner = styled.div`
-  background-color: #7395b0;
-  height: 200px;
-  border-radius: 20px;
+	background-color: #7395b0;
+	height: 200px;
+	border-radius: 20px;
 `
 const AvatarWrapper = styled.div`
-  position: absolute;
-  left: 50%;
-  transform: translate(-50%, -50%);
+	position: absolute;
+	left: 50%;
+	transform: translate(-50%, -50%);
 `
 const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 60px;
-  padding: 10px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	height: 60px;
+	padding: 10px;
 `
 
 const Text = styled.div``
 
 const Username = styled.h1`
-  font-weight: bold;
-  font-size: 1.5rem;
-  padding-bottom: 15px;
-  padding-top: 15px;
+	font-weight: bold;
+	font-size: 1.5rem;
+	padding-bottom: 15px;
+	padding-top: 15px;
 `
 
 const Joined = styled.h3`
-  font-size: 0.75rem;
+	font-size: 0.75rem;
 `
 
 const Buttons = styled.div`
-  padding-top: 5px;
+	padding-top: 5px;
 `
 
 const Bio = styled.div`
-  padding: 10px;
-  padding-top: 30px;
-  padding-bottom: 30px;
-  line-height: 1.25em;
+	padding: 10px;
+	padding-top: 30px;
+	padding-bottom: 30px;
+	line-height: 1.25em;
 `
 
 export default profilePage
